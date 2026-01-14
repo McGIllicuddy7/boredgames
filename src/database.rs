350b7c1514d9b17526184265ffcf0659684e39ed
Bridget Hartin Pasco <isaacpasco@icloud.com>
2026-01-14 10:09:52 -0800

    rewrite

--- src/database.rs
+++
@@ -1,9 +0,0 @@
-use std::{collections::HashMap, net::IpAddr};
-
-use serde::{Deserialize, Serialize};
-#[derive(Serialize, Deserialize)]
-pub struct DataBase {
-    pub user_name: String,
-    pub alias_table: HashMap<String, IpAddr>,
-    pub folder: String,
-}
