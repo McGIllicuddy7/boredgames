b13583b7b570048c6cbb845ced1c5614f03d8308
Bridget Hartin Pasco <isaacpasco@icloud.com>
2026-01-27 11:37:39 -0800

    f"

---
+++ src/id.rs
@@ -0,0 +1,124 @@
+pub const PAGE_SIZE: u64 = 2 << 28;
+pub const MAX_PAGE_COUNT: u64 = u64::MAX / PAGE_SIZE;
+pub use crate::rtils::marathon::ArachneId;
+pub use serde::{Deserialize, Serialize};
+pub use std::collections::{BTreeMap, BTreeSet};
+pub use std::sync::{Arc, Mutex};
+pub struct GlobalIdAllocatorInner {
+    pub resident_page_set: BTreeSet<u64>,
+    pub allocated_page_set: BTreeSet<u64>,
+}
+
+pub struct IdAllocatorInner {
+    pub start: u64,
+    pub used: BTreeSet<u64>,
+}
+
+impl GlobalIdAllocatorInner {
+    pub fn alloc_page(&mut self) -> u64 {
+        for i in 1..MAX_PAGE_COUNT {
+            if !self.allocated_page_set.contains(&i) && !self.resident_page_set.contains(&i) {
+                self.allocated_page_set.insert(i);
+                self.resident_page_set.insert(i);
+                return i;
+            }
+        }
+        panic!("no pages remaining");
+    }
+
+    pub fn free_page(&mut self, start: u64) {
+        self.allocated_page_set.remove(&start);
+    }
+
+    pub fn collect_garbage<U: ArachneId, T>(&mut self, map: &BTreeMap<U, T>) {
+        for (key, _) in map {
+            let page = (key.get() / PAGE_SIZE) * PAGE_SIZE;
+            if !self.allocated_page_set.contains(&page) {
+                if self.resident_page_set.contains(&page) {
+                    self.resident_page_set.remove(&page);
+                }
+            }
+        }
+    }
+}
+
+impl IdAllocatorInner {
+    pub fn alloc_id(&mut self) -> u64 {
+        for i in self.start..self.start + PAGE_SIZE {
+            if !self.used.contains(&i) {
+                self.used.insert(i);
+                return i;
+            }
+        }
+        panic!("no ids remaining");
+    }
+    pub fn free_id(&mut self, id: u64) {
+        self.used.remove(&id);
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Hash, Ord, Serialize, Debug, Deserialize)]
+#[repr(transparent)]
+pub struct GlobalId {
+    inner: u64,
+}
+
+impl ArachneId for GlobalId {
+    fn create(x: u64) -> Self {
+        Self { inner: x }
+    }
+
+    fn get(&self) -> u64 {
+        self.inner
+    }
+}
+
+pub struct IdPageAllocator {
+    inner: Mutex<GlobalIdAllocatorInner>,
+}
+
+impl IdPageAllocator {
+    pub const fn new() -> Self {
+        Self {
+            inner: Mutex::new(GlobalIdAllocatorInner {
+                resident_page_set: BTreeSet::new(),
+                allocated_page_set: BTreeSet::new(),
+            }),
+        }
+    }
+
+    pub fn alloc_page(&self) -> u64 {
+        self.inner.lock().unwrap().alloc_page()
+    }
+
+    pub fn free_page(&self, start: u64) {
+        self.inner.lock().unwrap().free_page(start);
+    }
+
+    pub fn collect_garbage<U: ArachneId, T>(&self, map: &BTreeMap<U, T>) {
+        self.inner.lock().unwrap().collect_garbage(map);
+    }
+}
+
+pub struct IdAllocator {
+    inner: Mutex<IdAllocatorInner>,
+}
+
+impl IdAllocator {
+    pub const fn new(start: u64) -> Self {
+        Self {
+            inner: Mutex::new(IdAllocatorInner {
+                start,
+                used: BTreeSet::new(),
+            }),
+        }
+    }
+
+    pub fn alloc_id(&self) -> GlobalId {
+        GlobalId::create(self.inner.lock().unwrap().alloc_id())
+    }
+
+    pub fn free_id(&self, id: GlobalId) {
+        self.inner.lock().unwrap().free_id(id.get());
+    }
+}
