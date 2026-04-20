4b89cebe4ac194d52ba98ece1bfd8c8f1bd027f9
Bridget Hartin Pasco <isaacpasco@icloud.com>
2026-04-20 13:48:40 -0700

    restarting

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-use crate::dos::{SysHandle, setup};
-
-pub mod client;
-pub mod dos;
-pub mod id;
-pub mod rtils;
-pub mod state;
-pub mod voip;
-pub fn testing123() -> i32 {
-    24
-}
-global!(x:i32 = testing123());
-pub fn main() {
-    if std::env::args().len() > 1 {
-        state::run_text_mode();
-    }
-    setup(main_func);
-}
-pub fn main_func(mut handle: SysHandle) {
-    client::run_client(&mut handle);
+fn main() {
+    println!("Hello, world!");
 }
